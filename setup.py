79e9d662fc9069d17c530daa230db05ce7a558d6
Noel Burton-Krahn <noel@burton-krahn.com>
2017-01-16 13:46:38 -0800

    battlesnake/game.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+#
+# Copyright 2016, Cisco Systems
+# All Rights Reserved.
+#
+
+import os
+import setuptools
+
+from pip.download import PipSession
+from pip.req import parse_requirements
+
+def reqs(path):
+    return [str(r.req) for r in parse_requirements(path, session=PipSession())
+            if r.req]
+
+INSTALL_REQUIRES = reqs("requirements.txt")
+TESTS_REQUIRE = reqs("test-requirements.txt")
+
+setuptools.setup(
+    name='battlesnake',
+    version='0.1.0',
+    url='https://github.com/noelbk/battlesnake-python',
+    description='Battlesnake Python client',
+    author='Noel Burton-Krahn <noel@burton-krahn.com>',
+    author_email='noel@burton-krahn.com',
+    packages=setuptools.find_packages(exclude=['tests']),
+    package_dir={'battlesnake': 'battlesnake'},
+    include_package_data=True,
+    install_requires=INSTALL_REQUIRES,
+    tests_require=TESTS_REQUIRE,
+    extras_require=dict(
+        test=TESTS_REQUIRE,
+        tests=TESTS_REQUIRE,
+        ),
+)
